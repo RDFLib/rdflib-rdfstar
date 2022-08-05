1d017fd8c30cde6e28abd56beed9bea808794afb
alooba1 <u6250082@anu.edu.au>
2022-08-05 16:33:53 +1000

    update serializer with collection and blanknodepropertylist results

--- processor_serialize.py
+++ processor_serialize.py
@@ -17,6 +17,7 @@ from rdflib.serializer import Serializer
 import rdflib
 from rdflib import URIRef
 from rdflib.namespace import RDF
+from rdflib.namespace import FOAF
 
 register(
     "ttls",
@@ -47,19 +48,68 @@ g.parse("test/turtle-star/turtle-star-syntax-inside-02.ttl", format = "ttls")
 #     ))
 # print(unreified_g.serialize())
 
+def expand_Bnode(node, g, dictionary, properties):
+    for s, p, o in g.triples((node, None, None)):
+        #todo () and []
+        # oList = properties.get(p, [])
+        # oList.append(o)
+        print("atatat", s,p, o)
+        print("ptype", type(p))
+        if ("http://www.w3.org/1999/02/22-rdf-syntax-ns#first" in p) or ("http://www.w3.org/1999/02/22-rdf-syntax-ns#rest" in p):
+            if o in dictionary:
+                properties.append(dictionary[o])
+            # elif isinstance(o, rdflib.term.BNode):
+            #     expand_Bnode(o, g, dictionary,properties)
+            # else:
+            #     properties.append(o)
+            else:
+                expand_Bnode(o, g, dictionary,properties)
+        else:
+            properties.append(p)
+            if o in dictionary:
+                properties.append(dictionary[o])
+            # elif isinstance(o, rdflib.term.BNode):
+            #     expand_Bnode(o, g, dictionary,properties)
+            # else:
+            #     properties.append(o)
+            else:
+                expand_Bnode(o, g, dictionary,properties)
+
+
+    return properties
+
 dictionary = dict()
 result = ""
 for s in g.subjects(predicate=RDF.type, object=RDF.Statement):
-    print(s)
-    print(
-       ( g.value(s, RDF.subject),
-       g.value(s, RDF.predicate),
-      g.value(s, RDF.object))
-      )
+    # print(s)
+    # print(
+    #    ( g.value(s, RDF.subject),
+    #    g.value(s, RDF.predicate),
+    #   g.value(s, RDF.object))
+    #   )
 
     subject = g.value(s, RDF.subject)
     predicate = g.value(s, RDF.predicate)
     object = g.value(s, RDF.object)
+
+    print("typetest", subject, type(subject), "\n")
+    print("current dict", dictionary, "\n")
+    properties = []
+    # all_changed = True
+    # while all_changed:
+        # for s, p, o in g.triples((subject, None, None)):
+        #     # oList = properties.get(p, [])
+        #     # oList.append(o)
+        #     if o in dictionary:
+        #         properties.append(dictionary(o))
+        #     else:
+    result = expand_Bnode(subject,g,dictionary,properties)
+
+    print("expand", result, "\n")
+
+    # all_changed = False
+    # while (all_changed==False):
+
 
     if isinstance(g.value(s, RDF.subject), rdflib.term.BNode):
         if subject in dictionary:
@@ -75,7 +125,7 @@ for s in g.subjects(predicate=RDF.type, object=RDF.Statement):
 
     # predicate = "<"+str(predicate)+">"
 
-    print("before", subject, type(subject))
+    # print("before", subject, type(subject))
     if (isinstance(subject, rdflib.term.URIRef)):
         subject = "<"+str(subject)+">"
 
@@ -89,14 +139,15 @@ for s in g.subjects(predicate=RDF.type, object=RDF.Statement):
 
     dictionary[s] = "<< "+str(subject)+ str(predicate)+str(object)+" >>"
 
-    print(type(g.value(s, RDF.object))) # why hash value is changed?
+    # print(type(g.value(s, RDF.object))) # why hash value is changed?
 
     # print("dictionary", dictionary)
 
-    print("test", subject ,predicate,object)
+    print("serialize", subject ,predicate,object)
     # if s == g.value(s, RDF.object):
     #     print("equal")
 
 # Todo: how to put this into serializer?
 
 # Todo: how to read in the blanknode to process collection and blank node property list?
+# what to discuss with others?
